78dd9fca0a6aba6e97e9a0c9df5e0ea32e821d1c
anhducokok <pham.aduc11@gmail.com>
2025-07-06 00:07:23 +0700

    fix api endpoint error

--- src/components/practice/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as PracticeCard } from './PracticeCard';
-export { default as PracticeList } from './PracticeList';
-export { default as PracticeAttemptView } from './PracticeAttempt';
-export { default as PracticeResults } from './PracticeResults';
